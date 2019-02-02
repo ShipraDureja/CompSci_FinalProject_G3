437e90dca0f923edcc4fd3851b0ecd8ea0a0018c
ShipraDureja <44194598+ShipraDureja@users.noreply.github.com>
2019-02-02 10:30:04 +0100

    Calculated Lennard-Jonnes Potential
    Distance and Neighbor List Initially uploaded in wrong folder.

---
+++ Potential.py
@@ -0,0 +1,25 @@
+import numpy as np
+import Distances
+
+epsilon = 1.
+sigma = 1.
+cutoff = 3. * sigma
+
+class Potentials():
+    '''
+    Calculate Potential
+    '''
+    def __init__(self, particle_pos, box_size, boundary):
+        self.vector = Distances.distance_vectors(particle_pos, box_size, boundary)
+        self.distance = Distances.distances(self.vector)
+
+    def lj_potential(self):
+        particles = self.distance.shape[0]
+        potential = np.zeros_like(self.distance)
+        for i in range(particles):
+            for j in range(i+1, particles):
+                if (self.distance[i][j] <= 0 or self.distance[i][j] > cutoff):
+                    potential[i][j] = 0.
+                else:
+                    potential[i][j] = 4. * epsilon * (sigma ** 12 / self.distance[i][j] ** 12 - sigma ** 6 / self.distance[i][j] ** 6)
+        return np.sum(potential, axis = -1)
